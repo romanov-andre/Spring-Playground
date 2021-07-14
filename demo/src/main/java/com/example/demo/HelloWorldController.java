c8aa5af1d63cf58c632c55f10c8b07ac8faa7bfa
Romanov Andre <romanovandre05@gmail.com>
2021-07-14 12:14:44 -0400

    Spring Security Application

---
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String firstPage() {
+        return "Hello World";
+    }
+}
+
